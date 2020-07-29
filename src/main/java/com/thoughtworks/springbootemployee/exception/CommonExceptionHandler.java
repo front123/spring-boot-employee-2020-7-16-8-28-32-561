a21fd6779836b6115f9e8bcbd642d46376ff728a
Max Miao <miaoma2@oocl.com>
2020-07-29 20:15:26 +0800

    [spring-boot-employee][max,front] feature: add CommonExceptionHandler to catch Exception and change response status info

---
+++ src/main/java/com/thoughtworks/springbootemployee/exception/CommonExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.springbootemployee.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class CommonExceptionHandler {
+
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    void raiseEmployeeNotFoundException(){
+
+    }
+}
