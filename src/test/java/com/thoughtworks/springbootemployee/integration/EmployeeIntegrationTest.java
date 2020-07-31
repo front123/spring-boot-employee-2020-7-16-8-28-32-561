ba5f85be8c86d33301d15c81165088c89a82b122
Sumtudou <1163015195@qq.com>
2020-07-31 20:27:37 +0800

    [spring-boot-employee][jay,front] feature :pass should_return_ok_when_delete_a_employee_given_add_a_employee_and_delete_it

--- src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
@@ -1,25 +1,34 @@
 package com.thoughtworks.springbootemployee.integration;
 
+import com.thoughtworks.springbootemployee.exception.CompanyNotFoundException;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-public class EmployeeIntegrationTest extends CommonIntegrationTest{
+public class EmployeeIntegrationTest extends CommonIntegrationTest {
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
     @AfterEach
-    void clearRepository(){
+    void clearRepository() {
         employeeRepository.deleteAll();
+    }
+
+    void addACompanyOOCl() throws Exception {
+        String companyJsonStr = "{\"name\":\"oocl\"}";
+        mockMvc.perform(post("/companies").content(companyJsonStr).contentType(MediaType.APPLICATION_JSON));
+    }
+    void addAEmployeeJay() throws Exception{
+        String employeeJsonStr = "{\n" +"\"name\":\"Jay\",\n" +"\"age\": 15,\n" +"\"gender\": \"male\",\n" +"\"companyId\":1\n" +"}";
+        mockMvc.perform(post("/employees").content(employeeJsonStr).contentType(MediaType.APPLICATION_JSON));
     }
 
     @Test
@@ -28,19 +37,44 @@ public class EmployeeIntegrationTest extends CommonIntegrationTest{
     }
 
     @Test
-    void should_return_ok_when_get_employee_by_id_given_id_1() throws Exception{
-        mockMvc.perform(get("/employees/1")
+    void should_return_ok_when_get_employee_by_id_given_id_1() throws Exception {
+        //given
+        addACompanyOOCl();
+        addAEmployeeJay();
+        int employeeId= 1;
+
+        //when and then
+        mockMvc.perform(get("/employees/"+Integer.toString(employeeId))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("name").value("chengcheng"));
+                .andExpect(jsonPath("name").value("Jay"));
     }
 
     @Test
-    void should_return_ok_when_add_employee_given_a_company_and_a_employee() throws Exception {
-        String employeeJsonStr = "{\"id\":1,\"name\":\"chengcheng\",\"age\":54,\"gender\":\"male\",\"company\":{\"company_id\":1,\"name\":\"oocl\"}}";
+    void should_return_ok_when_add_employee_given_a_company_and_a_employee() throws Exception, CompanyNotFoundException {
+        //given
+        addACompanyOOCl();
+        //when and then
+        String employeeJsonStr = "{\n" +"\"name\":\"Jay\",\n" +"\"age\": 15,\n" +"\"gender\": \"male\",\n" +"\"companyId\":1\n" +"}";
         mockMvc.perform(post("/employees")
                 .content(employeeJsonStr).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("name").value("chengcheng"));
+                .andExpect(jsonPath("name").value("Jay"));
 
     }
+
+    @Test
+    void should_return_ok_when_delete_a_employee_given_add_a_employee_and_delete_it() throws Exception {
+        //given
+        addACompanyOOCl();
+        addAEmployeeJay();
+        int employeeId= 1;
+        //when and then
+        mockMvc.perform(delete("/employees/"+ Integer.toString(employeeId))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+
+
+
 }
